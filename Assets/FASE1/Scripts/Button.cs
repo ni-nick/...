cd6e885acb0db74420b7f65bbe898542b5eef241
ni-nick <nicolle_aline@hotmail.com>
2020-02-27 16:04:46 -0300

    Primeira Fase ok

---
+++ Assets/FASE1/Scripts/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+
+    public string LevelToLoad;
+
+    public void loadLevel()
+    {
+        //Load the level from LevelToLoad
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
---
+++ Assets/FASE1/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject PainelControle;
+  
+
+    public bool isPaused = false;
+    public bool play = false;
+  
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        if (play)
+        {
+            PainelControle.SetActive(false);
+            play = false;
+        }
+        else
+        {
+            PainelControle.SetActive(true);
+            play = true;
+        }
+
+
+    }
+
+
+
+    public void Pause()
+    {
+        if (isPaused)
+        {
+            PainelControle.SetActive(false);
+            isPaused = false;
+        }
+        else
+        {
+            PainelControle.SetActive(true);
+               isPaused= true;
+        }
+
+       
+    }
+
+}
