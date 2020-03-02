7ed630ae83398e88c36c5afce4ac11f654fc7e18
ni-nick <nicolle_aline@hotmail.com>
2020-03-02 10:59:56 -0300

    fase2 c/ponto de luz - 02/03/2019

---
+++ Assets/FASE2/Scripts/LuzPegarObjetos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzPegarObjetos : MonoBehaviour
+{
+    public Material material;
+    void Start()
+    {
+        
+        foreach(GameObject objeto in Object.FindObjectsOfType(typeof(GameObject)))
+        { 
+            if(objeto.tag != "MainCamera" && objeto.GetComponent<SpriteRenderer>() != null)
+            {
+                objeto.GetComponent<SpriteRenderer>().material = material;
+            }
+        }
+    }
+}
