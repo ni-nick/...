ed69c811b29b58972c8dcadaaa30c22c7f44a945
ni-nick <nicolle_aline@hotmail.com>
2020-03-17 21:09:28 -0300

    iascara - teste dialogo tuichi\teste fase 1&2 (2 não completa)/16/03/

---
+++ Assets/FASE2/Scripts/Dialogo2.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogo2 : MonoBehaviour
+{
+
+    public GameObject painelbox;
+    public TextAsset arquivo;
+    public string[] texto;
+    public Text textoMensagem;
+
+    private int fimdalinha;
+    private int linhaAtual;
+    public bool ativo;
+
+
+    void Start()
+    {
+        if (arquivo != null)
+        {
+            texto = (arquivo.text.Split('\n'));
+        }
+
+        if (fimdalinha == 0)
+        {
+            fimdalinha = texto.Length;
+        }
+        desabilitar();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            habilitar();
+
+            if (linhaAtual < fimdalinha)
+            {
+                textoMensagem.text = texto[linhaAtual];
+            }
+            if (painelbox.activeSelf)
+            {
+                linhaAtual += 1;
+            }
+        }
+        if (linhaAtual > fimdalinha)
+        {
+            linhaAtual = 0;
+            desabilitar();
+        }
+    }
+
+    void habilitar()
+    {
+        painelbox.SetActive(true);
+    }
+
+    void desabilitar()
+    {
+        painelbox.SetActive(false);
+    }
+
+
+}
