3e2fcbcc9d679b6eccd58bcc783b7dc0fc27c67c
ni-nick <nicolle_aline@hotmail.com>
2020-03-21 18:22:22 -0300

    ias (16/03 até 21/03) - FalaInimigo funcionando

---
+++ Assets/FASE1/Scripts/FalaInimigo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FalaInimigo : MonoBehaviour
+{
+
+        public bool ativar = false;
+        public GameObject canvasinimigo;
+        public GameObject inimigoteste;
+
+    void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter2D(Collider2D collider2D)
+        {
+            if (collider2D.transform.name == "Jogador")
+            {
+            ativar = true;
+
+            canvasinimigo.GetComponent<CanvasGroup>().alpha = 1;
+            canvasinimigo.SetActive(true);
+            Debug.Log("eu passo aqui mas eu não funciono");
+
+            }
+        }
+        void OnTriggerExit2D(Collider2D colliderr2D)
+        {
+            if (colliderr2D.transform.name == "Jogador")
+            {
+
+            ativar = false;
+            canvasinimigo.GetComponent<CanvasGroup>().alpha = 0;
+            Destroy(inimigoteste);
+        }
+        }
+            void OnGui()
+        {
+            if (ativar == true)
+            {
+            canvasinimigo.SetActive(true);
+            }
+        }
+
+    }
