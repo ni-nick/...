254cc7f4445e6854039dfdae07fd03355e9cdb2d
ni-nick <nicolle_aline@hotmail.com>
2020-04-07 00:55:45 -0300

    06&07/04 - função volume funcionando

---
+++ Assets/FASE2/Scripts/ControleVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleVolume : MonoBehaviour
+{
+
+    float volumeMaster;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void VolumeMaster( float volume )
+    {
+        volumeMaster = volume;
+        AudioListener.volume = volumeMaster;
+
+    }
+}
