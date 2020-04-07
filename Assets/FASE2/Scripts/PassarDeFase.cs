4d369ed80d79199b14220fda2ebb1f8109a3e922
ni-nick <nicolle_aline@hotmail.com>
2020-04-07 03:06:26 -0300

    ias&nick&and - 07/04 - passar de fase (portal) funcionando

---
+++ Assets/FASE2/Scripts/PassarDeFase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassarDeFase : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) // quando o objeto para passar de fase tocar no "Player"
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // ele pega a cena atual mais 1 que vai para próxima cena 
+        }
+
+    }
+}
