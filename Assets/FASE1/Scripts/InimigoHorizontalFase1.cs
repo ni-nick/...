511f64cec583dca4a12243c965fdcb056a58ca38
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-04-25 04:41:28 -0300

    ias - 25/04

--- Assets/FASE1/Scripts/InimigoHorizontalFase1.cs
+++ Assets/FASE1/Scripts/InimigoHorizontalFase1.cs
@@ -7,14 +7,6 @@ public class InimigoHorizontalFase1 : MonoBehaviour
     private bool collidde = false;
     private float move = -2;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(move, GetComponent<Rigidbody2D>().velocity.y);
@@ -33,7 +25,7 @@ public class InimigoHorizontalFase1 : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collider2D)
     {
-        if (collider2D.gameObject.CompareTag("plataforma"))
+        if (collider2D.gameObject.CompareTag("NGruda"))
         {
             collidde = true;
         }
@@ -41,7 +33,7 @@ public class InimigoHorizontalFase1 : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collider2D)
     {
-        if (collider2D.gameObject.CompareTag("plataforma"))
+        if (collider2D.gameObject.CompareTag("NGruda"))
         {
             collidde = false;
         }
