df476e03f889cb8c6ae649bb0f108a7096cd85a9
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-05-08 04:08:44 -0300

    ias - plataformas fase 2 - 08/05

---
+++ Assets/FASE1/Scripts/ScriptPlataforma2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPlataforma2 : MonoBehaviour // script para jogador virar filho da plataforma
+{
+
+    public GameObject player2;
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.gameObject == player2)
+        {
+            player2.transform.parent = transform;
+        }
+        
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject == player2)
+        {
+            player2.transform.parent = null;
+        }
+
+    }
+}
---
+++ Assets/FASE2/Scripts/ScripPlataformaVertical.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScripPlataformaVertical : MonoBehaviour
+{
+    private bool paraBaixo = true;
+
+    public float velocidade = 3f;
+
+    //pontos que a plataforma vai percorrer
+    public Transform pontoA;
+    public Transform pontoB;
+   
+    void Update()
+    {
+        if(transform.position.y > pontoA.position.y)
+        {
+            paraBaixo = true;
+        }
+        if (transform.position.y < pontoB.position.y)
+        {
+            paraBaixo = false;
+        }
+
+        if (paraBaixo)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - velocidade * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + velocidade * Time.deltaTime);
+        }
+    }
+}
