9ae4d413e2da203850549a7797d0e801b2eb71a4
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-05-21 01:46:59 -0300

    21/05

---
+++ Assets/FASE2/Scripts/LoadCutscene2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCutscene2 : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) // quando o objeto para passar de fase tocar no "Player"
+        {
+            SceneManager.LoadScene("DialogoDamadaMorte"); // ele pega a cena atual e encaminha para a cena da cutscene
+        }
+
+    }
+}
