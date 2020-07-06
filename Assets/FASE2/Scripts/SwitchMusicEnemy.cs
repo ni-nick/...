e8b0525315619139c2e56cedaefeb10bc199a7e3
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-07-05 23:06:40 -0300

    05/07

---
+++ Assets/FASE2/Scripts/SwitchMusicEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMusicEnemy : MonoBehaviour
+{
+
+    public AudioSource backMusic; // a musica de background do jogo 
+
+
+    public void ChangeBackMusic(AudioClip music)
+    {
+        backMusic.Stop();
+        backMusic.clip = music;
+        backMusic.Play();
+    } 
+}
---
+++ Assets/FASE2/Scripts/SwitchTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrigger : MonoBehaviour
+{
+
+    public AudioClip newTrack;
+
+    private SwitchMusicEnemy theAM;
+
+    void Start()
+    {
+        theAM = FindObjectOfType<SwitchMusicEnemy>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.tag == "Player")
+        {
+            if(newTrack != null)
+                theAM.ChangeBackMusic(newTrack);
+        }
+    }
+}
