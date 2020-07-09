1a412e70e3afb6d07fc83e119974520b0203400a
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-07-09 01:43:01 -0300

    09/07

---
+++ Assets/Scripts/AtivarJornal2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtivarJornal2 : MonoBehaviour
+{
+
+    public bool ativar = false;
+
+    public GameObject JornalCanvas;
+    public GameObject AtivaJornals;
+
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.transform.name == "Player")
+        {
+            ativar = true;
+
+            JornalCanvas.GetComponent<CanvasGroup>().alpha = 1;
+            JornalCanvas.SetActive(true);
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D colliderr2D)
+    {
+        if (colliderr2D.transform.name == "Player")
+        {
+
+            ativar = false;
+            JornalCanvas.GetComponent<CanvasGroup>().alpha = 0;
+            // Destroy(AtivaJornals);
+            //  Destroy(JornalCanvas);
+        }
+    }
+    void OnGui()
+    {
+        if (ativar == true)
+        {
+            JornalCanvas.SetActive(true);
+        }
+    }
+
+}
+
