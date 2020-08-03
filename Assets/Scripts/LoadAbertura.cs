d446a4e0681bd3f7bead892c5a5521b594057648
ni-nick <60885142+ni-nick@users.noreply.github.com>
2020-08-02 22:44:29 -0300

    02/08- sera quem vem ai?

---
+++ Assets/Scripts/LoadAbertura.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAbertura : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine("Wait");
+
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene("Abertura");
+    }
+}
+
